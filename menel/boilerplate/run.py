fb3e2249b0e89f149b4b79877b1b460432713b2e
piotr <morseusz@gmail.com>
2014-01-25 20:53:24 +0000

    Added run.py

--- menel/boilerplate/run.py
+++ menel/boilerplate/run.py
@@ -1,28 +1,70 @@
-import itertools
+from threading import Thread, Lock
+from queue import Queue
 from scrappers import *
 from menel.scrappers import BaseScrapper
 
 
-def get_all_scrappers():
-    scrappers = []
-    globs = globals()
-    for g in globs:
-        cls = globs[g]
+class Runner:
+    def __init__(self):
+        self.scrappers = []
+        self.get_all_scrappers()
+        self.scrappers_lock = Lock()
+        self.queue = Queue()
+
+    def get_all_scrappers(self):
+        globs = globals()
+        for g in globs:
+            cls = globs[g]
+            try:
+                if issubclass(cls, BaseScrapper):
+                    self.scrappers.append(cls())
+            except TypeError:
+                pass
+
+    def get_independent(self):
+        independent = []
+        for scrapper in self.scrappers:
+            if not scrapper.requires:
+                independent.append(scrapper)
+
+        for i in independent:
+            self.scrappers.remove(i)
+
+        return independent
+
+    def unblock_dependencies(self, to_unblock):
+        cls = type(to_unblock)
+        with self.scrappers_lock:
+            for scrapper in self.scrappers:
+                if cls not in scrapper.requires:
+                    continue
+                scrapper.requires.remove(cls)
+                if not scrapper.requires:
+                    self.queue.put(scrapper)
+                    self.scrappers.remove(scrapper)
+
+    def run_and_unblock_dependencies(self, scrapper):
         try:
-            if issubclass(cls, BaseScrapper):
-                scrappers.append(cls())
-        except TypeError:
-            pass
-    return scrappers
+            scrapper.scrape_all()
+        finally:
+            self.unblock_dependencies(scrapper)
+
+    def run(self):
+        threads = []
+        for i in self.get_independent():
+            self.queue.put(i)
+
+        while True:
+            scrapper = self.queue.get(1)
+            thread = Thread(target=self.run_and_unblock_dependencies(scrapper))
+            thread.start()
+            threads.append(thread)
+            if not self.scrappers:
+                break
+
+        for thread in threads:
+            thread.join()
 
 
-def get_independent(scrappers):
-    independent = []
-    for scrapper in scrappers:
-        if not scrapper.requires:
-            independent.append(scrapper)
-
-    for i in independent:
-        scrappers.remove(i)
-
-    return independent
+if __name__ == '__main__':
+    Runner.run()
--- menel/test/test_run.py
+++ menel/test/test_run.py
@@ -4,5 +4,5 @@ from menel.boilerplate import run
 
 class TestRun(unittest.TestCase):
     def test_get_all_scrappers(self):
+        print(dir(run))
         get_all_scrappers = copy.deepcopy(run.get_all_scrappers)
-        print(get_all_scrappers())
